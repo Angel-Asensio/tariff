4483f1ea5438b76e96ff5023e5d564858232f8fd
Angel <angel.asensio@gmail.com>
2019-12-16 12:50:42 +0100

    fix sb test

--- src/test/java/de/angelasensio/tariff/service/CalculatorServiceTest.java
+++ src/test/java/de/angelasensio/tariff/service/CalculatorServiceTest.java
@@ -9,14 +9,21 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import de.angelasensio.tariff.domain.Module;
 import de.angelasensio.tariff.domain.ModuleType;
 import de.angelasensio.tariff.domain.Policy;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorServiceTest {
 
-    private CalculatorService calculatorService = new CalculatorService();
+    @Autowired
+    private CalculatorService calculatorService;
 
     @Test
     public void emptyPolicyReturnsZero() {
